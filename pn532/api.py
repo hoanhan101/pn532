e0bf63517172d3f909a229f68027cafeaafac7d2
Hoanh An <hoanhan@bennington.edu>
2019-05-13 14:21:14 -0400

    update frame constructing api

--- pn532/api.py
+++ pn532/api.py
@@ -1,11 +1,12 @@
+# -*- coding: utf-8 -*-
 #
 # api.py contains a set of core functions
 #
 
+from . import register
 from .smbus2.smbus2 import SMBus, i2c_msg
-import time
 
-from pn532.register import *
+import time
 
 REST_INTERVAL = 0.5
 BLOCK_SIZE = 20
@@ -13,7 +14,7 @@ BLOCK_SIZE = 20
 class PN532(object):
     def __init__(self):
         # i2c device address
-        self.address = PN532_DEFAULT_ADDRESS
+        self.address = register.PN532_DEFAULT_ADDRESS
 
         # smbus object
         self.bus = SMBus(1)
@@ -23,7 +24,7 @@ class PN532(object):
         self.sam_config()
 
     def read(self):
-        """keep reading until a card is detected, return the reading afterward"""
+        """keep reading until a card is detected and its reading is returned"""
         self.in_list_passive_target()
 
         while True:
@@ -33,27 +34,17 @@ class PN532(object):
 
     def sam_config(self):
         """send SAMConfiguration command"""
-        sam_config = [0x00, 0x00, 0xff, 0x05, 0xfb, 0xd4, 0x14, 0x01, 0x01, 0x00, 0x16, 0x00]
-        self.write_addr(sam_config)
+        self.write_addr(construct_frame(
+            register.PN532_COMMAND_SAMCONFIGURATION, 0x01, 0x01, 0x00)
+        )
         self.read_addr(BLOCK_SIZE)
 
     def in_list_passive_target(self):
         """send InListPassiveTarget command"""
-        card_config = [0x00, 0x00, 0xff, 0x04, 0xfc, 0xd4, 0x4a, 0x01, 0x00, 0xe1, 0x00]
-        self.write_addr(card_config)
+        self.write_addr(construct_frame(
+            register.PN532_COMMAND_INLISTPASSIVETARGET, 0x01, 0x00)
+        )
         self.read_addr(BLOCK_SIZE)
-
-    def construct_frame(self, data):
-        # begin with 6-bytes frame structure
-        buf = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
-        buf[0] = PN532_PREAMBLE
-        buf[1] = PN532_STARTCODE1
-        buf[2] = PN532_STARTCODE2
-        buf[3] = len(data) + 1          # number of bytes in data and frame identifier field
-        buf[4] = (buf[3] & 0xFF) + 0x01 # packet length checksum
-        buf[5] = PN532_HOSTTOPN532
-
-        return buf
 
     def write_addr(self, data):
         """write to its own address with given block data"""
@@ -75,3 +66,24 @@ class PN532(object):
 
         print("read_addr:", buf)
         return buf
+
+def construct_frame(data):
+    """construct frame for communicating between host controller and pn532"""
+    # begin with 6-bytes frame structure
+    buf = [0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    buf[0] = register.PN532_PREAMBLE
+    buf[1] = register.PN532_STARTCODE1
+    buf[2] = register.PN532_STARTCODE2
+    buf[3] = len(data) + 1           # number of bytes in data and frame identifier field
+    buf[4] = (~buf[3] & 0xFF) + 0x01 # packet length checksum
+    buf[5] = register.PN532_HOSTTOPN532
+
+    tmp_sum = register.PN532_HOSTTOPN532
+    for b in data:
+        tmp_sum += b
+        buf.append(b)
+
+    buf.append((~tmp_sum & 0xFF) + 0x01) # data checksum
+    buf.append(register.PN532_POSTAMBLE)
+
+    return buf
