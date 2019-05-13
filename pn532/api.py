2e563c7c4fc02089ef5c2b454f0d64bd7a066a03
Hoanh An <hoanhan@bennington.edu>
2019-05-13 02:47:29 +0000

    initial works for sam config

--- pn532/api.py
+++ pn532/api.py
@@ -7,6 +7,9 @@ import time
 
 from pn532.register import *
 
+REST_INTERVAL = 0.5
+BLOCK_SIZE = 20
+
 class PN532(object):
     def __init__(self):
         # i2c device address
@@ -15,12 +18,23 @@ class PN532(object):
         # smbus object
         self.bus = SMBus(1)
 
+    def sam_config(self):
+        """send samconfig command"""
+        sam_config = [0x00, 0x00, 0xff, 0x05, 0xfb, 0xd4, 0x14, 0x01, 0x01, 0x00, 0x16, 0x00]
+        self.write(sam_config)
+        self.read(BLOCK_SIZE)
+
     def write(self, data):
         """write to its own address with given block data"""
+        time.sleep(REST_INTERVAL)
+
         self.bus.write_i2c_block_data(self.address, self.address, data)
+        print("write:", data)
 
     def read(self, length):
         """read from its own address a given-length of block data"""
+        time.sleep(REST_INTERVAL)
+
         buf = []
         msg = i2c_msg.read(self.address, length)
         self.bus.i2c_rdwr(msg)
@@ -28,4 +42,5 @@ class PN532(object):
         for b in msg:
             buf.append(b)
 
+        print("read:", buf)
         return buf
